8c9bcd27f789b7fbbcd60f6f472a45b5b2ffbefd
Lea L <leandrooleones@gmail.com>
2024-03-25 17:30:34 -0300

    fix: userFeed instagram

--- app/lib/getPostIg.ts
+++ app/lib/getPostIg.ts
@@ -7,10 +7,11 @@ export default async function GetPost() {
     const ig = new IgApiClient();
     ig.state.generateDevice("pruebartmp");
     await ig.simulate.preLoginFlow();
+
     const loggedInUser = await ig.account.login("pruebartmp", "lolcito123");
     const userFeed = ig.feed.user(loggedInUser.pk)
     const myPostsFirstPage: UserFeedResponseItemsItem[] = await userFeed.items();
     console.log("*********", myPostsFirstPage.length)
-
+const a = userFeed.items()
 
 }
